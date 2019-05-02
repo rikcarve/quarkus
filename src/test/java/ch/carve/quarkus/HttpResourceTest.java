a29450151350b2f2b4651ce99f9a22720f5b59b1
rikcarve <arik.dasen@outlook.com>
2019-05-02 21:31:38 +0200

    improvements

--- src/test/java/ch/carve/quarkus/HttpResourceTest.java
+++ src/test/java/ch/carve/quarkus/HttpResourceTest.java
@@ -1,10 +1,11 @@
 package ch.carve.quarkus;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class HttpResourceTest {
@@ -15,7 +16,7 @@ public class HttpResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("Hello"));
     }
 
 }
--- src/test/java/ch/carve/quarkus/NativeHttpResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ch.carve.quarkus;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeHttpResourceIT extends HttpResourceTest {
-
-    // Execute the same tests but in native mode.
-}
