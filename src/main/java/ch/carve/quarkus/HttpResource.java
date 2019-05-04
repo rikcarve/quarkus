5d9390ef3a041c3bb59828d9de3ee95ec37314c6
rikcarve <arik.dasen@outlook.com>
2019-05-04 13:35:27 +0200

    json logging

--- src/main/java/ch/carve/quarkus/HttpResource.java
+++ src/main/java/ch/carve/quarkus/HttpResource.java
@@ -1,5 +1,8 @@
 package ch.carve.quarkus;
 
+import java.util.logging.Handler;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,6 +10,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logmanager.Level;
+import org.jboss.logmanager.LogContext;
+import org.jboss.logmanager.formatters.JsonFormatter;
+import org.jboss.logmanager.handlers.ConsoleHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +28,13 @@ public class HttpResource {
     @Inject
     @Stream("scs")
     Emitter<String> emitter;
+    
+    @PostConstruct
+    public void init() {
+        ConsoleHandler handler = new ConsoleHandler(new JsonFormatter());
+        handler.setLevel(Level.INFO);
+        LogContext.getLogContext().getLogger("").setHandlers(new Handler[] {handler});
+    }
     
     @POST
     @Produces(MediaType.APPLICATION_XML)
