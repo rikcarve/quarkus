b93b53f7624470fdf57873f0c21fee21e9a69b19
rikcarve <arik.dasen@outlook.com>
2019-10-19 10:45:48 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # quarkus
 Quarkus playground
+
+### Keycloak
+
+##### start in docker
+```
+docker run -d -p 8084:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=1234 --name keycloak jboss/keycloak
+```
+
+##### Setup
+https://kodnito.com/posts/microprofile-jwt-with-keycloak/
+
+##### create token with postman
+```
+POST /auth/realms/carve/protocol/openid-connect/token HTTP/1.1
+Host: localhost:8084
+Cache-Control: no-cache
+Content-Type: application/x-www-form-urlencoded
+
+realm=carve&grant_type=password&client_id=quarkus&username=rik&password=1234
+```
--- src/main/java/ch/carve/quarkus/SecuredResource.java
+++ src/main/java/ch/carve/quarkus/SecuredResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
 
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("secured")
@@ -24,7 +25,7 @@ public class SecuredResource {
         String name = caller == null ? "anonymous" : caller.getName();
         boolean hasJWT = jwt != null;
         String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, context.isSecure(), context.getAuthenticationScheme(), hasJWT);
-        return helloReply;
+        return helloReply + " " + jwt.getClaim(Claims.groups.name());
     }
 
     @GET
